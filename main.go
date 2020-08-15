96cd0520ba27007f5b47f0088bb1107126bf1085
Wisnu WH <wisnuwh22@gmail.com>
2020-08-15 17:19:20 +0700

    Setup recipe create recipe.go file create type for recipe struct and sliece create func to read recipe from json file

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+// import (
+// 	"encoding/json"
+// 	"log"
+// 	"net/http"
+// )
 
+// func ingredientPage(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	i := readIngredientJsonFile()
+// 	json.NewEncoder(w).Encode(i)
+// }
+
+// func recipePage(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// }
+
+// func handleRequest() {
+// 	http.HandleFunc("/ingredient", ingredientPage)
+// 	http.HandleFunc("/recipe", recipePage)
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// }
+
+func main() {
+	// 	handleRequest()
 }
--- recipe.go
+++ recipe.go
@@ -1,8 +1,34 @@
 package main
 
-type Recipe struct {
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type recipe struct {
 	Title       string   `json:"Title"`
 	Ingredients []string `json:"Ingredients"`
 }
 
-type Recipes []Recipe
+type recipes []recipe
+
+func readRecipeJsonFile() recipes {
+	bs, err := ioutil.ReadFile("recipess/data.json")
+	if err != nil {
+		fmt.Println("Error : ", err)
+		os.Exit(0)
+	}
+
+	r := recipes{}
+	err = json.Unmarshal([]byte(bs), &r)
+
+	if err != nil {
+		fmt.Println("Error : ", err)
+		os.Exit(0)
+	}
+
+	return r
+
+}
